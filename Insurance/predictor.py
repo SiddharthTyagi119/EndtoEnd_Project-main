607c05b2702ecc8ab8d60b80692fba907e7deb2c
siddharth Tyagi <sidtyagi05@gmail.com>
2023-03-29 00:54:39 +0530

    model evaluation-1

--- Insurance/predictor.py
+++ Insurance/predictor.py
@@ -1,3 +1,10 @@
+#creating a class that will create a folder after running the model on a new data and will save the new model in that folder.
+#folder to save new model and we have to call all the classes of all the files
+#comparison new model vs old model
+#accept or reject
+
+
+
 import os
 from Insurance.entity.config_entity import TRANSFORMER_OBJECT_FILE_NAME,MODEL_FILE_NAME,TARGET_ENCODER_OBJECT_FILE_NAME
 from glob import glob
@@ -15,30 +22,40 @@ class ModelResolver:
                 model_dir_name = "model"):
 
         self.model_registry=model_registry
+        #creating a dir to save the new model
         os.makedirs(self.model_registry,exist_ok=True)
+        #dir to store the transformed data files
         self.transformer_dir_name = transformer_dir_name
+        #dir to store encoded data
         self.target_encoder_dir_name=target_encoder_dir_name
+        #define model 
         self.model_dir_name=model_dir_name
-
 # 1
+#just creating a new directory to save the new model
+#saved model>>>0 with pickle file then 1,2 and so on...
+#the nummber  dir inc if we keep traing the model on new data 
     def get_latest_dir_path(self)->Optional[str]:
         try:
+            #defining model registry dir 
             dir_names = os.listdir(self.model_registry)
             if len(dir_names)==0:
                 return None
+            #creating the dir with count 1 as if we run the data 1st on new data and so on
             dir_names = list(map(int,dir_names))
+            #latest dir will be the one who run at last time
             latest_dir_name = max(dir_names)
             return os.path.join(self.model_registry,f"{latest_dir_name}")
         except Exception as e:
             raise e
 
-# 2
+# 2  
 
     def get_latest_model_path(self):
         try:
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
                 raise Exception(f"Model is not available")
+                #defining latest model
             return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
             raise e
@@ -48,6 +65,7 @@ class ModelResolver:
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
                 raise Exception(f"Transformer is not available")
+                #latest dir with transformed data path
             return os.path.join(latest_dir,self.transformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
         except Exception as e:
             raise e
@@ -57,15 +75,17 @@ class ModelResolver:
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
                 raise Exception(f"Target encoder is not available")
+                #latest dir with transformed encoded data path
             return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
             raise e
 
-# 5
+# 5 saving the latest directory
     def get_latest_save_dir_path(self)->str:
         try:
             latest_dir = self.get_latest_dir_path()
             if latest_dir==None:
+                #creating folder
                 return os.path.join(self.model_registry,f"{0}")
             latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
             return os.path.join(self.model_registry,f"{latest_dir_num+1}")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
 from Insurance.components.data_transformation import DataTransformation
 from Insurance.components.model_trainer import ModelTrainer
-#from Insurance.components.model_evaluation import ModelEvaluation
+from Insurance.components.model_evaluation import ModelEvaluation
 #from Insurance.components.model_pusher import ModelPusher
 
 
@@ -61,6 +61,15 @@ if __name__=="__main__":
        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+       #model evaluation
+       model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+       model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+       data_ingestion_artifact=data_ingestion_artifact,
+       data_transformation_artifact=data_transformation_artifact,
+       model_trainer_artifact=model_trainer_artifact)
+       model_eval_artifact = model_eval.initiate_model_evaluation()
+
 
 
       
